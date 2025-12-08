8fec1cedd6251f4cbc61a81a716ef8ce1310780f
Andrew Nguyen <andrnyen@gmail.com>
2025-12-09 10:43:46 +1100

    added firebase configuration keys

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDleZP68U5BZFEY7CgDa08hYAq6DmBOhM8",
+  authDomain: "borrow-3ccfb.firebaseapp.com",
+  projectId: "borrow-3ccfb",
+  storageBucket: "borrow-3ccfb.firebasestorage.app",
+  messagingSenderId: "528917038886",
+  appId: "1:528917038886:web:ffaf4e315aa698379663e7",
 };
 
 // Initialize Firebase
