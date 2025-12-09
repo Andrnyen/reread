2522029585dd3849c6e1545762bc94fb896bf1f3
Andrew Nguyen <andrnyen@gmail.com>
2025-12-09 18:55:45 +1100

    removed vercel node

--- frontend/api/group.ts
+++ frontend/api/group.ts
@@ -1,7 +1,6 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { proxyFetch } from "./utils";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   try {
     const chapterId = req.query.chapterId;
 
--- frontend/api/manga-list.ts
+++ frontend/api/manga-list.ts
@@ -1,7 +1,6 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import axios from "axios";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   try {
     const endpoint = req.query.endpoint;
 
